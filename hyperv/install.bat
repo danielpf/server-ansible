1dceaefd0af33e0875e92d8caf29eb3c6a8ee840
danielpf <daniel.fazeres@ukg.com>
2023-01-28 20:32:47 +0000

    testing

--- hyperv/install.bat
+++ hyperv/install.bat
@@ -2,8 +2,9 @@ set mac=00-15-5d-01-8a-0f
 set username=danielf
 set keyfile=~/.ssh/danielpf
 
+arp -a ^| findstr %mac%'
 for /f %%i in ('arp -a ^| findstr %mac%') do set ip=%%i
-echo "ip: %ip%"
+if "%ip%" == "" exit
 scp -i %keyfile% -r server-ansible "%username%@%ip%:~"
 
 ssh -t -i %keyfile% "%username%@%ip%" "cd server-ansible && sudo apt-get install dos2unix && dos2unix ./hyperv/install.sh && chmod +x ./hyperv/install.sh && ./hyperv/install.sh"
